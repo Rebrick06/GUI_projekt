bf8414490f6b190a58897129fccd1cb207ca1ba5
Harry <harrydah29@gmail.com>
2026-05-19 09:50:11 +0200

    Updated google fonts

--- imat/lib/pages/main_view.dart
+++ imat/lib/pages/main_view.dart
@@ -3,7 +3,6 @@ import 'package:imat_app/app_theme.dart';
 import 'package:imat_app/model/imat_data_handler.dart';
 import 'package:imat_app/widgets/product_card.dart';
 import 'package:provider/provider.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class MainView extends StatelessWidget {
   const MainView({super.key});
@@ -17,7 +16,6 @@ class MainView extends StatelessWidget {
     // Den kan vara enklare att förstå.
     // Denna version har fördelen att kort skapas on-demand.
     return Scaffold(
-      
       body: Column( 
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -34,31 +32,29 @@ class MainView extends StatelessWidget {
                   style: AppTheme.titleFont
                 )
               ],
-            )
+            ),
           ),
           
-            
-              Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(AppTheme.paddingSmall),
-                child: GridView.builder(
-                  itemCount: products.length,
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 4, // 4 kolumner
-                    crossAxisSpacing: AppTheme.paddingSmall,
-                    mainAxisSpacing: AppTheme.paddingSmall,
-                    childAspectRatio: 4 / 3,
-                  ),
-                  itemBuilder: (context, index) {
-                    final product = products[index];
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(AppTheme.paddingSmall),
+              child: GridView.builder(
+                itemCount: products.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4, // 4 kolumner
+                  crossAxisSpacing: AppTheme.paddingSmall,
+                  mainAxisSpacing: AppTheme.paddingSmall,
+                  childAspectRatio: 4 / 3,
+                ),
+                itemBuilder: (context, index) {
+                  final product = products[index];
 
-                    return ProductCard(product, iMat);
-                  },
+                  return ProductCard(product, iMat);
+                },
               ),
             ),
           ),
           
-        
           Container(
             color: Color.fromRGBO(176, 230, 185, 1), 
             height: 20,
