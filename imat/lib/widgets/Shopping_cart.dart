ea23acc5a022325878e5943238cf351e7e5fe71a
Dalendonut <fabianpe@chalmers.se>
2026-05-19 15:52:04 +0200

    Shopping cart widegt

---
+++ imat/lib/widgets/Shopping_cart.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ShoppingCart extends StatelessWidget {
+  const ShoppingCart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  } 
+}
+
