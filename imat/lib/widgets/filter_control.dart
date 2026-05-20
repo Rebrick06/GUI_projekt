beb233777859af8bfec4a0715f1a5c23756dc2b3
Harry <harrydah29@gmail.com>
2026-05-20 12:47:07 +0200

    Inga buggar kategori (trust)

--- imat/lib/widgets/filter_control.dart
+++ imat/lib/widgets/filter_control.dart
@@ -2,27 +2,59 @@ import 'package:flutter/material.dart';
 import 'package:imat_app/app_theme.dart';
 import 'package:imat_app/model/imat/product.dart';
 
-class FilterControl extends StatelessWidget {
+class FilterControl extends StatefulWidget {
   const FilterControl({super.key});
 
-@override
-Widget build(BuildContext context) {
+  @override
+  State<FilterControl> createState() => _FilterControlState();
+}
 
-return Card(
-child: Padding(
-padding:EdgeInsets.all(AppTheme.paddingSmall),
-                child:Column                                                      (
-children: ProductCategory.values.map((category) {
-            return ListTile(
-              title:  Text(category.name),
-              onTap:                    (){
-                                Placeholder();
-      },
-            );
-  }).toList(),
+class _FilterControlState extends State<FilterControl>{
+  bool showAll = false;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(AppTheme.paddingSmall),
+        child: Column(
+          children: [
+            Text("Kategorier", style: AppTheme.titleFont),
+          
+
+            Wrap(
+              spacing: AppTheme.paddingSmall,
+              runSpacing: AppTheme.paddingSmall,
+
+              children: (
+                showAll ? ProductCategory.values: 
+                ProductCategory.values.take(5)).map((category){
+                return ActionChip(
+                  label: Text(category.name),
+                  backgroundColor: AppTheme.darkColor,
+                  onPressed: () {
+
+                  },
+                );
+              }).toList(),
             ),
-                                  )
-                  );
+            Center(
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    showAll = !showAll;
+                  });
+                },
+                child: Text(
+                  showAll ? "USH FY BORT!" : "JAG VILL HA MEERRR!",
+                ),
+              ),
+            ),
+          ],
+        ),
+      )
+    );
     
-                }
-                  }
+  }
+}
